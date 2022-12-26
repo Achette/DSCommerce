edadef7bc5fdba9c9de5f89124652b475b8b95d8
Achette <i.achette@live.com>
2022-12-26 20:19:10 -0300

    Refact: adjusted Catalog request and add error handling

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,16 +2,34 @@ import { SearchBar } from "../../../components/SearchBar";
 import { CatalogCard } from "../../../components/CatalogCard";
 import { ButtonNextPage } from "../../../components/ButtonNextPage";
 import "./styles.css";
-import { products as Products } from "../../../services/product-service";
+import React from "react";
+import { ProductDTOProps } from "../../../types";
+import { ApiProducts } from "../../../services/api";
 
 export const Catalog = () => {
+  const [products, setProducts] = React.useState<ProductDTOProps[]>([]);
+
+  const fetchData = React.useCallback(async () => {
+    try {
+      const prods = await ApiProducts.getAll();
+
+      setProducts(prods);
+    } catch (e) {
+      alert("Erro ao fazer requisição");
+    }
+  }, []);
+
+  React.useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          {Products.map((product) => (
+          {products.map((product) => (
             <CatalogCard key={product.id} product={product} />
           ))}
         </div>
