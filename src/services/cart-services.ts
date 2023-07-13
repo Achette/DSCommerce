885d22cbf5be90073f52a6ac6ab3a265b45ab3ec
Achette <i.achette@live.com>
2023-07-12 21:21:27 -0300

    refact: altera ordem das importações

--- src/services/cart-services.ts
+++ src/services/cart-services.ts
@@ -1,6 +1,6 @@
-import { OrderDTO, OrderItemDTO } from "../models/order";
-import * as cartRepository from "../localStorage/cart-Repository";
 import { ProductDTOProps } from "../types";
+import { OrderDTO, OrderItemDTO } from "../models/order";
+import * as cartRepository from "../localStorage/cart-repository";
 
 export const saveCart = (cart: OrderDTO) => {
   cartRepository.save(cart);
