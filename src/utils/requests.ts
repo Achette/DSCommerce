99075f5120fffbf54587d3307c69018f46d08229
Achette <i.achette@live.com>
2023-07-17 09:39:17 -0300

    feat: adiciona backendRequest para centralizar requisições

---
+++ src/utils/requests.ts
@@ -0,0 +1,6 @@
+import { BASE_URL } from "../constants/system";
+import axios, { AxiosRequestConfig } from "axios";
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+  return axios({ ...config, baseURL: BASE_URL });
+};
