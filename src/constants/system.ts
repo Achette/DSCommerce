f494495e6267a69c2d09e0f00756f65507fdb486
Achette <i.achette@live.com>
2023-07-18 14:31:18 -0300

    feat: adiciona variável de ambiente para token

--- src/constants/system.ts
+++ src/constants/system.ts
@@ -2,6 +2,8 @@ export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.dev.commerce/cart";
 
+export const TOKEN_KEY = "com.dev.commerce/Token";
+
 export const CLIENT_ID = "myclientid"
 
 export const CLIENT_SECRET = "myclientsecret"
