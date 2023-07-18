834fe5b62d9c5610bfcd9e8b78e012471bbf5e38
Achette <i.achette@live.com>
2023-07-18 12:20:20 -0300

    feat: adiciona funcionalidades e requisição na página de login

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,26 +1,54 @@
+import React from "react";
+import { CredentialsDTO } from "../../../models/auth";
+import * as authService from "../../../services/auth-service";
 import "./styles.css";
 
 export const Login = () => {
+  const [formData, setFormData] = React.useState<CredentialsDTO>({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    authService
+      .loginRequest(formData)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log("erro no login ", error));
+  };
+
   return (
     <main>
       <section id="login-section" className="dsc-container">
         <div className="dsc-login-form-container">
-          <form className="dsc-card dsc-form">
+          <form className="dsc-card dsc-form" onSubmit={(e) => handleSubmit(e)}>
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
                 <input
+                  name="username"
                   className="dsc-form-control"
                   type="text"
                   placeholder="Email"
+                  value={formData.username}
+                  onChange={(event) => handleInputChange(event)}
                 />
                 <div className="dsc-form-error"></div>
               </div>
               <div>
                 <input
+                  name="password"
                   className="dsc-form-control"
                   type="password"
                   placeholder="Senha"
+                  value={formData.password}
+                  onChange={(event) => handleInputChange(event)}
                 />
               </div>
             </div>
