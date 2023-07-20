bf20c9f147a48d52a756fe77bad5195e382b4009
Achette <i.achette@live.com>
2023-07-20 16:54:36 -0300

    feat: adiciona credentials no request backend

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,6 +1,14 @@
 import { BASE_URL } from '../constants/system'
 import axios, { AxiosRequestConfig } from 'axios'
+import { getAccessToken } from '../services/auth-service'
 
 export const requestBackend = (config: AxiosRequestConfig) => {
-  return axios({ ...config, baseURL: BASE_URL })
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer ' + getAccessToken(),
+      }
+    : config.headers
+
+  return axios({ ...config, baseURL: BASE_URL, headers })
 }
