49e8658d83fb34c0451e4cd6e3c5f5c6a711ebae
Achette <i.achette@live.com>
2023-07-20 11:40:52 -0300

    feat: adiciona requisição para usuário logado

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -1,11 +1,25 @@
+import React from 'react'
+import { UserDTO } from '../../../types'
+import * as userService from '../../../services/user-service'
 import './styles.css'
 
 export const AdminHome = () => {
+  const [user, setUser] = React.useState<UserDTO>()
+
+  React.useEffect(() => {
+    userService
+      .findMe()
+      .then((res) => setUser(res.data))
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
   return (
     <main>
       <section id="admin-home-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">
-          Bem-vindo à área administrativa
+          Bem-vindo à área administrativa {user?.name}
         </h2>
       </section>
     </main>
