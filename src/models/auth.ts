ad82f82657860008791d1bce25f195efe93a995b
Achette <i.achette@live.com>
2023-07-24 17:48:12 -0300

    refact: exporta type

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -3,7 +3,7 @@ export type CredentialsDTO = {
   password: string
 }
 
-type RoleEnum = 'ROLE_ADMIN' | 'ROLE_CLIENT'
+export type RoleEnum = 'ROLE_ADMIN' | 'ROLE_CLIENT'
 
 export type AccessTokenPayloadDTO = {
   exp: number
