e65224b4426e9db029edeebe74cb05680347d75d
Achette <i.achette@live.com>
2023-07-24 17:58:24 -0300

    feat: cria Access Control route level

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,12 +1,16 @@
 import { Navigate } from 'react-router-dom'
 import * as authService from '../../services/auth-service'
+import { RoleEnum } from '../../models/auth'
 
 type Props = {
   children: JSX.Element
+  roles?: RoleEnum[]
 }
 
-export const PrivateRoute = ({ children }: Props) => {
+export const PrivateRoute = ({ children, roles = [] }: Props) => {
   if (!authService.isAuthenticated()) return <Navigate to="/login" />
+
+  if (!authService.hasAnyRoles(roles)) return <Navigate to="/catalog" />
 
   return children
 }
