f53696e52c60542b783042ad0af1bd230a113087
Achette <i.achette@live.com>
2023-07-26 09:26:50 -0300

    feat: adiciona renderização condicional para usuários com role Admin

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,6 +1,9 @@
-import './styles.css'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import { CartIcon } from '../CartIcon'
+import { Link } from 'react-router-dom'
+import AdminIcon from '../../assets/img/admin.svg'
+import * as authService from '../../services/auth-service'
+import './styles.css'
 
 export const HeaderClient = () => {
   return (
@@ -11,6 +14,13 @@ export const HeaderClient = () => {
         </Link>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-items-container">
+            {authService.hasAnyRoles(['ROLE_ADMIN']) && (
+              <Link to="/admin">
+                <div className="dsc-menu-item">
+                  <img src={AdminIcon} alt="Admin" />
+                </div>
+              </Link>
+            )}
             <Link to="/cart">
               <div className="dsc-menu-item">
                 <CartIcon />
